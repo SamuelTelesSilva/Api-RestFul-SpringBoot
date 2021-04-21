69fac9867b752d91913f28041e69c1bb2c2acb7f
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-21 16:30:45 -0300

    Como usar o @PathVariable para passar e receber parametros via URL

--- carros/src/main/java/com/example/carros/api/IndexController.java
+++ carros/src/main/java/com/example/carros/api/IndexController.java
@@ -1,9 +1,7 @@
 package com.example.carros.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +17,28 @@ public class IndexController {
     }
 
     /**
-     * Aqui estou usando o @RequestParam para passar parametros via URL, ex: @RequestParam("login");
+     * Aqui estou usando o @PathVariable para passar parametros via URL, ex: @PathVariable("login");
      * como passar os parametros via URL usando o metodo GET (não é viavel passar senhas via URL):
-     * vai na URL e digite no final ?parametro=conteudo&parametro=conteudo
-     * exemplo: localhost:8080/login?login=samuel&senha=123
-     * sempre que tiver mais parametros voce add o & no final.
+     * vai na URL e digite no final /login/{login}/senha/{senha}
+     * exemplo: http://localhost:8080/login/samuel/senha/124
      * @param login
      * @param senha
      * @return
      */
-    @GetMapping("/login")
-    public String login(@RequestParam("login")  String login, @RequestParam("senha") String senha){
+    @GetMapping("/login/{login}/senha/{senha}")
+    public String login(@PathVariable("login")  String login, @PathVariable("senha") String senha){
         return "Login: " + login + ", Senha: " + senha;
+    }
+
+    //Exemplo pegando por id usando o @PathVariable
+    @GetMapping("/carro/{id}")
+    public String getCarroByid(@PathVariable("id") Long id){
+        return "Carro " + id;
+    }
+    //Exemplo pegando por tipo usando o @PathVariable
+    @GetMapping("/carro/tipo/{tipo}") 
+    public String getCarroByTipo(@PathVariable("tipo") String tipo){
+        return " Lista de Carros Carro " + "Tipo " + tipo;
     }
 
     
