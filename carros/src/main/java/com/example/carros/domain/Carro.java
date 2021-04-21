fe34494376ce72cb2fa8b72d15e194da6ff26efc
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-21 20:36:50 -0300

    Adicionado o Lombok para substituir os getters and setters entre outros

--- carros/src/main/java/com/example/carros/domain/Carro.java
+++ carros/src/main/java/com/example/carros/domain/Carro.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Carro {
 
     @Id
@@ -13,36 +16,6 @@ public class Carro {
     private Long id;
     private String nome;
     private String tipo;
-
-    public Carro() {
-
-    }
-    
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public Carro(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-    
-    public Long getId() {
-        return id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     
 }
