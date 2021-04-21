f31861dceefb430cb1b5c2d42b37af83f22ff6f3
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-21 16:34:42 -0300

    Passando parametros via Post

--- carros/src/main/java/com/example/carros/api/IndexController.java
+++ carros/src/main/java/com/example/carros/api/IndexController.java
@@ -2,6 +2,7 @@ package com.example.carros.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,32 +18,15 @@ public class IndexController {
     }
 
     /**
-     * Aqui estou usando o @PathVariable para passar parametros via URL, ex: @PathVariable("login");
-     * como passar os parametros via URL usando o metodo GET (não é viavel passar senhas via URL):
-     * vai na URL e digite no final /login/{login}/senha/{senha}
-     * exemplo: http://localhost:8080/login/samuel/senha/124
+     * Para passar parametros via post, voce não pode passar via URL tem que colocar os parametros no BODY
      * @param login
      * @param senha
      * @return
      */
-    @GetMapping("/login/{login}/senha/{senha}")
-    public String login(@PathVariable("login")  String login, @PathVariable("senha") String senha){
+    @PostMapping("/login")
+    public String login(@RequestParam("login")  String login, @RequestParam("senha") String senha){
         return "Login: " + login + ", Senha: " + senha;
     }
 
-    //Exemplo pegando por id usando o @PathVariable
-    @GetMapping("/carro/{id}")
-    public String getCarroByid(@PathVariable("id") Long id){
-        return "Carro " + id;
-    }
-    //Exemplo pegando por tipo usando o @PathVariable
-    @GetMapping("/carro/tipo/{tipo}") 
-    public String getCarroByTipo(@PathVariable("tipo") String tipo){
-        return " Lista de Carros Carro " + "Tipo " + tipo;
-    }
-
-    
-
-    
 
 }
