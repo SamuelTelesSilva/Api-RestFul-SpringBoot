208cc20a452c138df49d7d3f371499af223a48d3
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-22 16:03:53 -0300

    Banco de dados em memoria, H2, preenchendo o H2 em memoria

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,7 +1,16 @@
 package com.example.carros;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
+import com.example.carros.domain.dto.CarroDTO;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +26,66 @@ class CarrosApplicationTests {
 
 	@Test
 	public void teste(){
-		
+
 		Carro carro = new Carro();
-		carro.setNome("ferrari");
+		carro.setNome("Ferrari");
 		carro.setTipo("esportivos");
 
-		carroService.insert(carro);
+		CarroDTO c = carroService.insert(carro);
+
+		assertNotNull(c);
+
+		Long id = c.getId();
+		assertNotNull(id);
+
+		// buscar o objeto
+		Optional<CarroDTO> op = carroService.getCarrosById(id);
+		assertTrue(op.isPresent());
+
+		c = op.get();
+		assertEquals("Ferrari", c.getNome());
+		assertEquals("esportivos", c.getTipo());
+
+		// deletar objeto
+		carroService.delete(id);
+
+
+		//verificar se deletou
+		assertFalse(carroService.getCarrosById(id).isPresent());
+		
+
+
 	}
+	@Test
+    public void testLista() {
+
+        List<CarroDTO> carros = carroService.getCarros();
+
+        assertEquals(30, carros.size());
+    }
+
+	@Test
+    public void testGet() {
+
+        Optional<CarroDTO> op = carroService.getCarrosById(11L);
+
+        assertTrue(op.isPresent());
+
+        CarroDTO c = op.get();
+
+        assertEquals("Ferrari FF", c.getNome());
+    }
+
+	@Test
+    public void testListaPorTipo() {
+
+        assertEquals(10, carroService.getCarrosByTipo("classicos").size());
+        assertEquals(10, carroService.getCarrosByTipo("esportivos").size());
+        assertEquals(10, carroService.getCarrosByTipo("luxo").size());
+
+        assertEquals(0, carroService.getCarrosByTipo("x").size());
+    }
+
 
 
 
