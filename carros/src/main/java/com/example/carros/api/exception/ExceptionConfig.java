df9a36a51acd51791346ad5cef466b4cbdf60c66
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-22 20:13:52 -0300

    Memory Authentication e Perfis de acesso (ROLES)

--- carros/src/main/java/com/example/carros/api/exception/ExceptionConfig.java
+++ carros/src/main/java/com/example/carros/api/exception/ExceptionConfig.java
@@ -1,18 +1,11 @@
 package com.example.carros.api.exception;
 
-import net.bytebuddy.pool.TypePool;
-
-import java.io.Serializable;
-
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -21,29 +14,29 @@ public class ExceptionConfig extends ResponseEntityExceptionHandler {
     @ExceptionHandler({
             EmptyResultDataAccessException.class
     })
-    public ResponseEntity errorNotFound(Exception ex) {
+    public ResponseEntity errorNotFound() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler({
            IllegalArgumentException.class
     })
-    public ResponseEntity errorBadRequest(Exception ex) {
+    public ResponseEntity errorBadRequest() {
         return ResponseEntity.badRequest().build();
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity<>(new ExceptionError("Operação não permitida"), HttpStatus.METHOD_NOT_ALLOWED);
+    @ExceptionHandler({
+            AccessDeniedException.class
+    })
+    public ResponseEntity accessDenied() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error("Acesso negado"));
     }
 }
 
-class ExceptionError implements Serializable {
-    private String error;
-    ExceptionError(String error) {
+class Error {
+    public String error;
+
+    public Error(String error) {
         this.error = error;
-    }
-    public String getError() {
-        return error;
     }
 }
--- carros/src/main/java/com/example/carros/api/security/SecurityConfig.java
+++ carros/src/main/java/com/example/carros/api/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.example.carros.api.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
     @Override
@@ -16,5 +20,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        
+
+        auth
+            .inMemoryAuthentication().passwordEncoder(encoder)
+                .withUser("user").password(encoder.encode("user")).roles("USER")
+               .and()
+                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
     }
 }
