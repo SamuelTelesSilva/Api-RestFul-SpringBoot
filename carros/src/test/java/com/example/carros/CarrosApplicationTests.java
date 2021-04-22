7d4b4c87d839f2cc208d3266b1b7677808b7abb1
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-22 10:39:50 -0300

    teste para inserir um carro

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,10 +1,31 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CarrosApplicationTests {
+
+	@Autowired
+	private CarroService carroService;
+
+
+	@Test
+	public void teste(){
+		
+		Carro carro = new Carro();
+		carro.setNome("ferrari");
+		carro.setTipo("esportivos");
+
+		carroService.insert(carro);
+	}
+
+
 
 	@Test
 	void contextLoads() {
