f73853e04d2a89c3a5118f4b7d5e28dc1ee02373
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-22 21:34:30 -0300

    Database Authentication - User, pegando usuario do BD e colocando ADM ou USER

--- carros/src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
+++ carros/src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
@@ -1,17 +1,12 @@
 package com.example.carros.api.security;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.core.userdetails.User;
-
 import com.example.carros.domain.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.carros.domain.User;
 
 @Service(value = "userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -22,12 +17,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        com.example.carros.domain.User user =  userRep.findByLogin(username);
-
-        if(user == null){
+        User user = userRep.findByLogin(username);
+        if(user == null) {
             throw new UsernameNotFoundException("user not found");
         }
-        return User.withUsername(username).password(user.getSenha()).roles("USER").build();
+
+        return user;
 
     }
 }
---
+++ carros/src/main/java/com/example/carros/domain/Role.java
@@ -0,0 +1,24 @@
+package com.example.carros.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+}
