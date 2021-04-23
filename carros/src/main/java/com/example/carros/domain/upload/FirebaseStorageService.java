2248d1e2f281346414ca534c80bc2d89e5e984a1
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-23 17:40:59 -0300

    Utilizando o FIREBASE para mandar os arquivos

---
+++ carros/src/main/java/com/example/carros/domain/upload/FirebaseStorageService.java
@@ -0,0 +1,69 @@
+package com.example.carros.domain.upload;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import javax.annotation.PostConstruct;
+
+import com.example.carros.api.upload.UploadInput;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Acl;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+
+import org.springframework.stereotype.Service;
+
+//https://firebase.google.com/docs/storage/admin/start
+@Service
+public class FirebaseStorageService {
+
+    @PostConstruct
+    private void init() throws IOException {
+        if(FirebaseApp.getApps().isEmpty()) {
+            InputStream in =
+                    FirebaseStorageService.class.getResourceAsStream("/serviceAccountKey.json");
+
+            System.out.println(in);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(in))
+                    .setStorageBucket("api-carros-db.appspot.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
+
+    
+    public String upload(UploadInput uploadInput) {
+
+
+        Bucket bucket = StorageClient.getInstance().bucket();
+        System.out.println(bucket);
+
+//        Blob blob = bucket.create("nome.txt","Ricardo Ninja Lecheta".getBytes(), "text/html");
+
+        byte[] bytes = Base64.getDecoder().decode(uploadInput.getBase64());
+
+        String fileName = uploadInput.getFileName();
+        Blob blob = bucket.create(fileName,bytes, uploadInput.getMimeType());
+
+        // Assina URL válida por N dias
+//        URL signedUrl = blob.signUrl(3, TimeUnit.DAYS);
+
+        // Deixa URL pública
+        blob.createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+
+        return String.format("https://storage.googleapis.com/%s/%s",bucket.getName(),fileName);
+    }
+}
+
+
+/**
+ * Não pode deixar o data:image/png na base64 quando for enviar via JSON
+ * 
+ */
