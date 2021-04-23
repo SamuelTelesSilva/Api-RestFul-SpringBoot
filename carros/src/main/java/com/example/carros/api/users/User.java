994aadd78409709684bf4b4c5700c3955548f91d
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-04-23 20:22:46 -0300

    Adicionando o UsersController

--- carros/src/main/java/com/example/carros/api/users/User.java
+++ carros/src/main/java/com/example/carros/api/users/User.java
@@ -12,8 +12,7 @@ import java.util.List;
 
 @Entity
 @Data
-public class User implements UserDetails{
-
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ carros/src/main/java/com/example/carros/api/users/UserController.java
@@ -0,0 +1,32 @@
+package com.example.carros.api.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    
+    @Autowired
+    private UserService service;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<UserDTO> list = service.getUsers();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/info")
+    public UserDTO userInfo(@AuthenticationPrincipal User user) {
+
+        //UserDetails userDetails = JwtUtil.getUserDetails();
+
+        return UserDTO.create(user);
+    }
+}
